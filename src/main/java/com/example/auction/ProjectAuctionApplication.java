4b3c7962a7c5613fb0e8f9601023128dfb7e8699
Deon <Deon@Deon>
2023-09-12 22:02:33 +0900

    초기 세팅

---
+++ src/main/java/com/example/auction/ProjectAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAuctionApplication.class, args);
+	}
+
+}
