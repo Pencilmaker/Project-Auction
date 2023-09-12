c115f2cfdefceaae3d1e8cccf9ebdad7fd783b86
Deon <Deon@Deon>
2023-09-12 22:49:21 +0900

    homecontroller 생성

--- src/main/java/com/example/auction/controller/homeController.java
+++ src/main/java/com/example/auction/controller/homeController.java
@@ -1,6 +1,7 @@
 package com.example.auction.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,4 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class homeController {
 
+	@GetMapping
+	public String home() {
+		return "";
+	}
 }
