56d9e850cc92882d5bc9c2b99efa28fef50d7076
Deon <Deon@Deon>
2023-09-12 20:56:44 +0900

    mapper 추가

--- src/main/java/com/example/auction/controller/productController.java
+++ src/main/java/com/example/auction/controller/productController.java
@@ -2,16 +2,19 @@ package com.example.auction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-public class homeController {
 
-	@GetMapping("product")
+public class productController {
+
+	@GetMapping("{product_id}")
 	public String ProductForm() {
-		log.info("");
-		return "product/product";
+		log.info("ProductForm.html");
+		return "product/productForm";
 	}
+	
 }
